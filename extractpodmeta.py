ab0a69ffdc63d11c62426b62804a06272cd31e74
Michael <mycall.707@gmail.com>
2022-06-12 15:54:34 +0200

    Method to format dataframe and write to .csv

--- extractpodmeta.py
+++ extractpodmeta.py
@@ -14,6 +14,7 @@ class PodMetaData:
         self.url = url
         self.meta = None
         self.__fetch_meta__()
+        self.all_pods = None
 
     def __fetch_meta__(self):
         """
@@ -22,10 +23,10 @@ class PodMetaData:
         """
         self.meta = requests.get(self.url).json()["body"]["channels"]
 
-    def get_all_pods(self):
+    def __set_all_pods__(self):
         """
-        Method to return the metadata of all podcasts
-        :return: Return pd.dataframe
+        Method to retrieve the metadata of all podcasts.
+        :return:
         """
         eps_url = "https://api.audioboom.com/channels/4254821/audio_clips"
         episodes = []
@@ -36,10 +37,41 @@ class PodMetaData:
                 eps_url + f"?page[number]={page}&page[items]=150").json()
             new_results = eps_api.get("body")["audio_clips"]
             for item in new_results:
+                #TODO: Move this to format_pods
+                # Keep only plays of counts.
+                item["counts"] = item["counts"]["plays"]
+                # Keep only first url.
+                item["urls"] = item["urls"]["detail"]
                 episodes.append(item)
             page += 1
 
-        return pd.DataFrame(episodes)
+        self.all_pods = pd.DataFrame(episodes)
+
+    def get_all_pods(self, format=True):
+        """
+        Method to retrieve the metadata of all podcasts.
+        :param format: If true, format dataframe.
+        :return: pd.dataframe
+        """
+        self.__set_all_pods__()
+        if format == True:
+            self.__format_pods__()
+        return self.all_pods
+
+    def __format_pods__(self):
+        """
+        Method to format the metadata of all podcasts
+        :param dataframe: Dataframe to format
+        :return: Formatted dataframe
+        """
+        #Split title at episode number
+        self.all_pods[["episode_number", "title"]] = self.all_pods["title"].str.split(r"\.", 1,expand=True)
+        #Set duration by minutes.
+        self.all_pods["duration"] = self.all_pods["duration"].div(60).astype(int)
+        # Keep relevant columns and rearrange
+        self.all_pods = self.all_pods[
+            ["episode_number", "title", "description", "updated_at", "duration",
+             "uploaded_at", "counts", "urls"]]
 
     def get_latest_pod(self):
         """
@@ -57,7 +89,7 @@ def main():
     df = quemese.get_all_pods()
 
     # saving as tsv file
-    # df.to_csv('episodios.csv', sep="\t")
+    df.to_csv('episodios.csv', sep=",", encoding="utf8")
 
 if __name__ == "__main__":
     main()
